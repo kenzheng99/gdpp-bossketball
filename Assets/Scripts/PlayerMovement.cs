271b863391f4f08cbb555eee2843820a74a5699f
bchen0707 <bchen0707@gmail.com>
2023-02-15 23:58:23 -0500

    implemented player dash (left shift)
    BUG: I have a bool canDash that when I touch the ground is set to true as a way to reset the dash but the player can infinitely dash on the ground (see discord)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
     [SerializeField] private float movementSpeed;
@@ -9,8 +10,19 @@ public class PlayerMovement : MonoBehaviour {
     private bool touchingFloor;
     private bool canDoubleJump;
 
+    [Header("Dashing")]
+    [SerializeField] private float dashVelocity;
+    [SerializeField] private float dashTime;
+    [SerializeField] private float dashCooldown;
+    [SerializeField] private int dashCharges = 1;
+    private Vector2 dashingDir;
+    private bool isDashing;
+    private bool canDash = true;
+    private TrailRenderer trailRenderer;
+    
     void Start() {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        trailRenderer = GetComponent<TrailRenderer>();
     }
 
     void Update() {
@@ -24,6 +36,44 @@ public class PlayerMovement : MonoBehaviour {
                 canDoubleJump = false;
             }
         }
+        //dashing code
+        var dashInput = Input.GetButtonDown("Dash");
+        if (dashInput && canDash)
+        {
+            Dash();
+        }
+
+        if (isDashing)
+        {
+            rb.velocity = dashingDir.normalized * dashVelocity; 
+        }
+        if (touchingFloor)
+        {
+            dashCharges = 1;
+            canDash = true;
+            // StartCoroutine(DashCooldown());
+        }
+    }
+
+    private void Dash()
+    {
+        isDashing = true;
+        canDash = false;
+        trailRenderer.emitting = true;
+        dashingDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (dashingDir == Vector2.zero)
+        {
+            dashingDir = new Vector2(transform.localScale.x, 0);
+        }
+        StartCoroutine(StopDashing());
+    }
+
+    private IEnumerator StopDashing()
+    {
+        yield return new WaitForSeconds(dashTime);
+        trailRenderer.emitting = false;
+        isDashing = false;
+        dashCharges -= 1;
     }
 
     private void Move(float inputX) {
