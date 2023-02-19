82f1dd8e58937a145bc6aeda72543e36b817ace2
bchen0707 <bchen0707@gmail.com>
2023-02-18 20:42:57 -0500

    input music when player presses start

--- Assets/Scripts/UI/StartScreen.cs
+++ Assets/Scripts/UI/StartScreen.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartScreen : MonoBehaviour {
-    
+
     public void StartGame() {
         SceneManager.LoadScene("SampleScene");
+        SoundManager.Instance.StartMusic();
     }
     public void Quit() {
         Application.Quit();
