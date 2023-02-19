1ebf6e4a8f1d6ccb4a2e29ee5f96d0d0da685848
bchen0707 <bchen0707@gmail.com>
2023-02-18 21:30:26 -0500

    implemented UI button press and shooting sfx

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -17,8 +17,9 @@ public class SoundManager : MonoBehaviour
     public AudioClip bossHoopDestroyedSFX;
     public AudioClip bossEnteringPhaseTwoSFX;
     public AudioClip bossDeathSFX;
-    public AudioClip succesfulShotSFX;
-    
+    public AudioClip succesfulShotSFX;    
+
+    public AudioClip buttonSelectUISFX;
 
     private void Start()
     {
