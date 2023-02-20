e7a76cfdac001e218443e47c8375f9313cb0f31a
joshHuang02 <johuang10@gmail.com>
2023-02-19 19:29:48 -0500

    made the boss move during homing attack

--- Assets/Scripts/Boss/HomingAttackState.cs
+++ Assets/Scripts/Boss/HomingAttackState.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [CreateAssetMenu(menuName = "BossStates/HomingAttackState")]
 public class HomingAttackState: BossState {
@@ -15,7 +17,12 @@ public class HomingAttackState: BossState {
     [SerializeField] private Vector2 targetPosition;
     [SerializeField] private float randOffSet;
     [SerializeField] private float trajectoryDelay;
+    
+    [SerializeField] private float waitTimeSeconds;
     [SerializeField] private Bounds moveBounds;
+    [SerializeField] private float directionTimeSeconds;
+    [SerializeField] private float moveDirectionDelta;
+    [SerializeField] private float moveSpeed;
 
     private GameObject boss;
     private Vector3 fireDir;
@@ -25,12 +32,17 @@ public class HomingAttackState: BossState {
     private int numFired;
     private Vector2 currentVelocity;
     
+    private Timer directionTimer;
+    private Vector3 moveDir;
+    private float dirDelta;
+    
     private Transform playerTr;
     private List<GameObject> projectiles;
+    private bool arrivedAtStart;
 
     public override void EnterState(BossStateMachine stateMachine) {
         Debug.Log("HomingAttackState");
-        if (GameManager._instance.bossPhaseTwo == true)
+        if (GameManager.Instance.bossPhaseTwo == true)
         {
             SoundManager.Instance.PlayBossGenericRoarSound();
         }
@@ -46,12 +58,35 @@ public class HomingAttackState: BossState {
         playerTr = GameObject.FindGameObjectWithTag("Player").transform;
         projectiles = new List<GameObject>();
         trajectoryTimer = new Timer(trajectoryDelay);
+        
+        directionTimer = new Timer(directionTimeSeconds);
+        moveDir = Vector3.down;
+        dirDelta = moveDirectionDelta;
     }
 
     public override void UpdateState(BossStateMachine stateMachine) {
 
-        if (Vector2.Distance(boss.transform.position, targetPosition) < 0.5) {
+        if (!arrivedAtStart && Vector2.Distance(boss.transform.position, targetPosition) < 0.5) {
+            arrivedAtStart = true;
+        }
+        if (arrivedAtStart) {
             // arrived, start attack
+            if (directionTimer.Done()) {
+                dirDelta = Random.Range(-moveDirectionDelta, moveDirectionDelta);
+                directionTimer = new Timer(waitTimeSeconds);
+            }
+
+            var bossPos = boss.transform.position;
+            if (Math.Abs(bossPos.x) > moveBounds.max.x) {
+                moveDir.x *= -1;
+            } else if (bossPos.y > moveBounds.max.y || bossPos.y < moveBounds.min.y) {
+                moveDir.y *= -1;
+            }
+        
+            moveDir = Quaternion.Euler(0, 0, dirDelta) * moveDir;
+            boss.transform.Translate(moveSpeed * Time.deltaTime * moveDir);
+            directionTimer.Tick(Time.deltaTime);
+            
             if (fireCoolDown.Done() && numFired < numAttacks*numProjectiles) {
                 projectiles.Add(FireProjectile(fireDir));
                 fireCoolDown = new Timer(coolDown);
@@ -60,7 +95,7 @@ public class HomingAttackState: BossState {
             fireCoolDown.Tick(Time.deltaTime);
         }
         else {
-            // move to spot
+            // move to starting position
             Vector2 newPosition = Vector2.SmoothDamp(boss.transform.position, targetPosition, ref currentVelocity,
                 bossMoveTime);
             boss.transform.position = newPosition;
